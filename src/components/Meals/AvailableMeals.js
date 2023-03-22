bc93d314748c0bbf0aa3cc0e6f1813394259a7f9
Tiago Gomes <tidgomes@gmail.com>
2023-03-22 10:58:48 -0300

    create MealItem component ad its module.css file, also use it in AvailableMeals component

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,3 +1,4 @@
+import MealItem from './MealItem/MealItem'
 import Card from '../UI/Card'
 import classes from './AvailableMeals.module.css'
 
@@ -29,7 +30,14 @@ const DUMMY_MEALS = [
 ]
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map(meal => <li key={meal.id}>{meal.name}</li>)
+  const mealsList = DUMMY_MEALS.map(meal => (
+    <MealItem
+      key={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ))
 
   return (
     <section className={classes.meals}>
