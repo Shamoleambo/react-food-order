0a6fb1ea2c6abbc0f1a11ac9465e85cfe0f93a46
Tiago Gomes <tidgomes@gmail.com>
2023-03-22 13:31:09 -0300

    use reducer to create a state for CartItems and the amount

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,12 +1,39 @@
+import { useReducer } from 'react'
 import CartContext from './cart-context'
 
+const defaultCartState = { items: [], totalAmount: 0 }
+
+const cartReducer = (state, action) => {
+  if (action === 'ADD') {
+    const itemPrice = action.item.price
+    const itemAmount = action.item.amount
+
+    const items = state.items.concat(action.item)
+    const totalAmount = state.totalAmount + itemPrice * itemAmount
+
+    return { items, totalAmount }
+  } else if (action === 'REMOVE') {
+  }
+
+  return defaultCartState
+}
+
 const CartProvider = props => {
-  const addItemHandler = item => {}
-  const removeItemHandler = id => {}
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  )
+
+  const addItemHandler = item => {
+    dispatchCartAction({ type: 'ADD', item })
+  }
+  const removeItemHandler = id => {
+    dispatchCartAction({ type: 'REMOVE', id })
+  }
 
   const cartContext = {
-    items: [],
-    amount: 0,
+    items: cartState.items,
+    amount: cartState.totalAmount,
     addItem: addItemHandler,
     removeItem: removeItemHandler
   }
